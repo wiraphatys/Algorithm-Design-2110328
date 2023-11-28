87b91aeca09a8be769cea14475c51e35d8943b4b
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2023-11-29 01:24:37 +0700

    :skull:

--- chula grader/a62_q2a_line_graph/a62_q2a_line_graph.cpp
+++ chula grader/a62_q2a_line_graph/a62_q2a_line_graph.cpp
@@ -1,12 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> adjList[200001];
+vector<bool> visited(200001);
+int c = 0;
+
+void DFS(int n) {
+
+}
+
 int main() {
-    int vertices, edges, a, b;
+    int vertices, edges;
     cin >> vertices >> edges;
-    
     for (int i = 0; i < edges; ++i) {
         cin >> a >> b;
+        adjList[a].push_back(b);
+        adjList[b].push_back(a);
+    }
+    for (int i = 0; i < vertices; ++i) {
+        if (!visited[i]) DFS(i);
     }
     return 0;
 }
