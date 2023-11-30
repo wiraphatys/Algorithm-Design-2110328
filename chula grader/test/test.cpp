835bb03be8d57a359dc965b18e9f2f5abb569d89
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2023-11-30 22:11:51 +0700

    testing unsigned long long

--- chula grader/test/test.cpp
+++ chula grader/test/test.cpp
@@ -1,8 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int c = 0;
+
+void A(double n) {
+    if (n <= 1) return;
+    A(sqrt(n));
+    c++;
+}
+
 int main() {
-    int x = MAX_INT;
-    cout << x;
+    unsigned long long x = -1;
+    A(x);
+    cout << c;
     return 0;
 }
