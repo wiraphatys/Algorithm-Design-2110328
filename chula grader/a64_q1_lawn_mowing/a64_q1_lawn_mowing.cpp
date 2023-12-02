9f912d19610845c95e7d7ff205232766d0775ece
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2023-12-02 17:31:01 +0700

    updated lawn mowing

--- chula grader/a64_q1_lawn_mowing/a64_q1_lawn_mowing.cpp
+++ chula grader/a64_q1_lawn_mowing/a64_q1_lawn_mowing.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 typedef long long ll;
 int main() {
-    int n, m, k, sum = 0;
+    int n, m, k, x;
+    ll sum = 0;
     cin >> n >> m >> k;
-    vector<int> A(n), S(n,0);
-    for (int i = 0; i < n; ++i) cin >> A[i];
-    S[0] = A[0]+k;
-    for (int i = 1; i < n; ++i) S[i] = S[i-1]+A[i]+k;
+    vector<ll> s(n+1), cost(n+1);
+    for (int i = 1; i <= n; ++i) {
+        cin >> x;
+        sum += x;
+        s[i] = sum;
+        cost[i] = s[i]+i*k;
+    }
     while (m--) {
-        int left;
-        ll budget;
-        cin >> left >> budget;
-        if (left > 0) budget += S[left-1];
-        auto it = --(upper_bound(S.begin(), S.end(), budget));
-        int x = it - (S.begin()+left-1);
-        int result = (left == 0) ? (*it-k*x) : (*it-(k*x+S[left-1]));
-        cout << result << "\n";
+        int start, budget;
+        cin >> start >> budget;
+        int t = cost[start];
+        budget += t;
+        auto it = --(upper_bound(cost.begin(), cost.end(), budget));
+        auto idx = it - cost.begin();
+        cout << s[idx]-s[start] << "\n";
     }
     return 0;
 }
-
-// A[n] = 1 2 3 4 5
-// S[n] = 1 3 6 10 15
